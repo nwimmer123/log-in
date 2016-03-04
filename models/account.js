2853daf169f22258790252bb9987665fa10952db
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-04 14:36:23 -0800

    removed unnecesry models

--- models/account.js
+++ models/account.js
@@ -2,12 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
-var Book = require('./book');
-
 var Account = new Schema({
     username: String,
     password: String,
-    books: [Book.schema]
 });
 
 Account.plugin(passportLocalMongoose);
--- models/index.js
+++ models/index.js
@@ -1,15 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/read-it');
-//heroku connection
-// mongoose.connect( process.env.MONGOLAB_URI ||
-//                   process.env.MONGOHQ_URL ||
-//                   "mongodb://localhost/read-it_test" );
+mongoose.connect('mongodb://localhost/log-in');
 
-var Book = require('./book');
-var Review = require('./review');
 var Account = require('./account');
 
-module.exports.Book = require('./book.js');
-
 module.exports.Account = Account;
-module.exports.Review = Review;
